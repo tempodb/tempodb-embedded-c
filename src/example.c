14545443758e0eb04f50e02850144551e4f6d16c
Patrick Schless <patrick@tempo-db.com>
2012-10-16 16:23:38 -0500

    use write_by_key

--- src/example.c
+++ src/example.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
   tempodb_create("key", "secret");
 
   char *response_buffer = (char *)malloc(255);
-  tempodb_write_by_id("seriesid3", 10, response_buffer, 255);
+  tempodb_write_by_key("my_favorite_series", 10, response_buffer, 255);
   printf("Response: %s\n", response_buffer);
 
   tempodb_destroy();
