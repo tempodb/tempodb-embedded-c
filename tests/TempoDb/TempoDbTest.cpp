ca66cf96f8fcd591555ab051fba7ddc3d8eacaa4
Patrick Schless <patrick@plainlystated.com>
2012-10-28 20:48:36 -0500

    testing travis ci failures

--- tests/TempoDb/TempoDbTest.cpp
+++ tests/TempoDb/TempoDbTest.cpp
@@ -116,7 +116,7 @@ TEST(tempodb, Write_Response_Overrun)
   tempodb_config *config = tempodb_create("my_access_key", "my_secret_key");
   char *response_buffer = (char *)malloc(255);
   memset(response_buffer, 1, 255);
-  set_test_response("200 OK");
+  set_test_response("100 OK");
   tempodb_write_by_id(config, "series_id", 10, response_buffer, 3);
   STRCMP_EQUAL("20", response_buffer);
   free(response_buffer);
